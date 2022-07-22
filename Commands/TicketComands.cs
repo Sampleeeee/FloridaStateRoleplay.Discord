e1003f7227941a83642ff4b272ce39c33ef654b0
Sample <sample@bluewaterrp.com>
2022-07-22 02:19:07 -0400

    z

--- Commands/TicketComands.cs
+++ Commands/TicketComands.cs
@@ -11,31 +11,6 @@ public class TicketComands : ApplicationCommandModule
     [SlashCommand( "test", "testing ticket command" )]
     public async Task TestTicketCommandAsync( InteractionContext ctx )
     {
-        var options = new List<DiscordSelectComponentOption>()
-        {
-            new DiscordSelectComponentOption(
-                "Label, no description",
-                "label_no_desc"),
-
-            new DiscordSelectComponentOption(
-                "Label, Description",
-                "label_with_desc",
-                "This is a description!"),
-
-            new DiscordSelectComponentOption(
-                "Label, Description, Emoji",
-                "label_with_desc_emoji",
-                "This is a description!",
-                emoji: new DiscordComponentEmoji(854260064906117121)),
-
-            new DiscordSelectComponentOption(
-                "Label, Description, Emoji (Default)",
-                "label_with_desc_emoji_default",
-                "This is a description!",
-                isDefault: true,
-                new DiscordComponentEmoji(854260064906117121))
-        };
-
         foreach ( var type in Config.Current.TicketTypes )
             if ( !string.IsNullOrWhiteSpace( type.Emoji ) )
                 options.Add( new DiscordSelectComponentOption( type.Title, type.Title, null, false,
--- Entities/Config.cs
+++ Entities/Config.cs
@@ -145,17 +145,17 @@ public class Config
 
     public readonly TicketType[] TicketTypes =
     {
-        new( 917912578179604545, 917912578464841747, "General Support", "", ":speech_left:" ),
+        new( 917912578179604545, 917912578464841747, "General Support", "", "💬" ),
         new( 917912578179604545, 917912578464841747, "Ban Appeal", 
             "Please send a screenshot of your ban and and a staff member will be with you shortly.", 
-            ":page_facing_up:" ),
-        new( 917912578213154911, 955541854487707728, "Management Team", "", ":crown:" ),
-        new( 551918087352614952, 956279565750243388, "Development Team", "", ":hammer:" ),
+            "📄" ),
+        new( 917912578213154911, 955541854487707728, "Management Team", "", "👑" ),
+        new( 551918087352614952, 956279565750243388, "Development Team", "", "🔨" ),
         new( 917912578146074640, 917912578464841747, "Florida Department of Law Enforcement", "",
-            ":oncoming_police_car:" ),
-        new( 917912578120904731, 917912578464841747, "Civilian Department", "", ":adult:" ),
-        new( 917912578146074637, 917912578464841747, "Hillsborough Fire Department", "", ":fire:" ),
-        new( 954356034040897608, 917912578464841747, "Communications Department", "", ":radio:" ),
+            "🚔" ),
+        new( 917912578120904731, 917912578464841747, "Civilian Department", "", "🧑" ),
+        new( 917912578146074637, 917912578464841747, "Hillsborough Fire Department", "", "🔥" ),
+        new( 954356034040897608, 917912578464841747, "Communications Department", "", "📻" ),
     };
 
     private bool _levelingEnabled = true;
