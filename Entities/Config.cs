2af4175c67994f8c98efb08b6e4d0b4ce9df7b61
Sample <sample@bluewaterrp.com>
2022-07-22 02:04:20 -0400

    emojis

--- Entities/Config.cs
+++ Entities/Config.cs
@@ -145,16 +145,16 @@ public class Config
 
     public readonly TicketType[] TicketTypes =
     {
-        new( 917912578179604545, 917912578464841747, "General Support", "", 999916672318185492 ),
+        new( 917912578179604545, 917912578464841747, "General Support", "", 999919553456898108 ),
         new( 917912578179604545, 917912578464841747, "Ban Appeal",
             "Please send a screenshot of your ban and and a staff member will be with you shortly.",
-            999916672318185492 ),
-        new( 917912578213154911, 955541854487707728, "Management Team", "", 999916865340059708 ),
-        new( 551918087352614952, 956279565750243388, "Development Team", "", 999916672318185492 ),
-        new( 917912578146074640, 917912578464841747, "Florida Department of Law Enforcement", "", 999916672318185492 ),
-        new( 917912578146074637, 917912578464841747, "Hillsborough Fire Department", "", 999916672318185492 ),
-        new( 954356034040897608, 917912578464841747, "Communications Department", "", 999916672318185492 ),
-        new( 917912578120904731, 917912578464841747, "Civilian Department", "", 999916672318185492 )
+            999919553456898108 ),
+        new( 917912578213154911, 955541854487707728, "Management Team", "", 999919553456898108 ),
+        new( 551918087352614952, 956279565750243388, "Development Team", "", 999919553456898108 ),
+        new( 917912578146074640, 917912578464841747, "Florida Department of Law Enforcement", "", 999919553456898108 ),
+        new( 917912578120904731, 917912578464841747, "Civilian Department", "", 999919553456898108 ),
+        new( 917912578146074637, 917912578464841747, "Hillsborough Fire Department", "", 999919553456898108 ),
+        new( 954356034040897608, 917912578464841747, "Communications Department", "", 999919553456898108 ),
     };
 
     private bool _levelingEnabled = true;
